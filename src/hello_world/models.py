606d781f80114ade47b10d6a70f482fd132671b4
Yehor <egorsapon@gmail.com>
2022-07-14 17:16:17 +0300

    by VSCode tutor

--- src/hello_world/models.py
+++ src/hello_world/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PublishingHous(models.Model):
+    name = models.CharField(
+        verbose_name="Publishing Hous's name",
+        max_length=50,
+        )
+    description = models.TextField(
+        verbose_name="Description",
+        blank=True,
+        null=True
+        )
--- src/proj/urls.py
+++ src/proj/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from hello_world import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello_world_view),
+    path("", include("hello_world.urls")),
 ]
