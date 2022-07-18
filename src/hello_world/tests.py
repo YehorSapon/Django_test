55af741938de42c67b2b13c71c359263ee93d233
Yehor Sapon <egorsapon@gmail.com>
2022-07-19 00:03:30 +0300

    Failed test_home_page_returns

--- src/hello_world/tests.py
+++ src/hello_world/tests.py
@@ -1,9 +1,19 @@
+from hello_world.views import hello_world_view
 from django.test import TestCase
+from django.urls import resolve
+from django.shortcuts import render
 
-# Create your tests here.
 
+class HomePageTest(TestCase):
 
-class SmokeTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, hello_world_view)
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_home_page_returns_correct_html(self):
+        #request = self.hello_world_view(request, 'hello_world/hello.html')
+        response = hello_world_view(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Hello World! Django test</title>', html)
+        self.assertTrue(html.endswith('</html>'))
