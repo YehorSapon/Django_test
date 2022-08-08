cf4c1bd7128da62f29513a4eba90b4ed73a5b7e1
Yehor <egorsapon@gmail.com>
2022-08-08 15:33:22 +0300

    monday

--- src/y_bookside/views.py
+++ src/y_bookside/views.py
@@ -2,21 +2,32 @@ from django.utils.timezone import datetime
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from y_bookside.forms import LogMessageForm
+from book import models as bmodels
+
 
 class HomePage(TemplateView):
     template_name = "y_bookside/home.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["date"] = datetime.now
+        context["book_list"] = bmodels.BookCard.objects.all()
+        return context
+
+
+class CaruselHomePage(TemplateView):
+    template_name = "templates/base_carusel.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["book_list"] = bmodels.BookCard.objects.all()
         return context
 
 
 def page_about(request):
     context = {
-            'date': datetime.now(),
-            'title': 'About',
-            }
+        'date': datetime.now(),
+        'title': 'About',
+    }
     return render(request,
                   'y_bookside/about.html',
                   context=context)
@@ -24,9 +35,9 @@ def page_about(request):
 
 def page_contacts(request):
     context = {
-            'date': datetime.now(),
-            'title': 'Contacts',
-            }
+        'date': datetime.now(),
+        'title': 'Contacts',
+    }
     return render(request,
                   'y_bookside/contacts.html',
                   context=context)
