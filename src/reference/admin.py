3f25aad78fe429ebdb97ec2a884ba74cb2e367a7
Yehor Sapon <egorsapon@gmail.com>
2022-08-12 23:47:24 +0300

    author model new register in Admin panel

--- src/reference/admin.py
+++ src/reference/admin.py
@@ -3,7 +3,15 @@ from reference import models
 
 # Register your models here.
 
-admin.site.register(models.Author)
+
 admin.site.register(models.PublishingHous)
 admin.site.register(models.Genre_book)
 admin.site.register(models.Series_book)
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'second_name', 'surname', 'date_birth', 'date_death')
+    list_filter = ('name', 'surname')
+    search_fields = ('name', 'surname')
