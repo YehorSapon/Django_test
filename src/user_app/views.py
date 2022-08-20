bcf35642e6a0f757c47578e184c12976aaf1dfc6
Alena Sapon <saponalena@gmail.com>
2022-08-21 00:38:43 +0300

    add in user profile

--- src/user_app/views.py
+++ src/user_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 
-class ProfileView(TemplateView):
+class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = "user_app/profile.html"
+    model = models.User
+    success_url = reverse_lazy("user_app:profile")
+    login_url = reverse_lazy("user_app:login")
     
